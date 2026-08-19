37b5ea41913dcc93bfc9c1350625a2d747dac6aa
xxdl <xxdl@xxdl.top>
2026-08-19 15:06:10 +0800

    修复重复导入问题

--- shared/types/common/index.ts
+++ shared/types/common/index.ts
@@ -1,8 +1,3 @@
 export * from './ApiReq'
 export * from './ApiResp'
 export * from './TRPCFormattedError'
-
-// 重新导出关键类型，方便使用
-export type { ApiReq, ApiQueryReq, ApiReqWithSort } from './ApiReq'
-export type { ApiPageResp, TrpcPageResp } from './ApiResp'
-export type { TRPCFormattedError } from './TRPCFormattedError'
